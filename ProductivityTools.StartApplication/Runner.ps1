f5c9a2c22ed45711614f82eef088d4bb55428ade
Pawel Wujczyk <pwujczyk@gmail.com>
2026-02-13 22:28:05 +0100

    test link

--- ProductivityTools.StartApplication/Runner.ps1
+++ ProductivityTools.StartApplication/Runner.ps1
@@ -3,5 +3,5 @@ Write-Host "pawel"
 Import-Module D:\GitHub\ProductivityTools.PSStartApplication\ProductivityTools.StartApplication\ProductivityTools.StartApplication.psm1 -Force
 #Import-Module D:\GitHub\PSGet-Configuration\Get-Configuration\Get-Configuration.psm1 -force
 #Set-StartApplicationConfigurationPath d:\Trash
-Start-Application notepad
+Start-Application conemu
 #Start-Application notepad -kill
